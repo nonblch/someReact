d8622262ecbb88e8ce44a3b6586112b9c724a048
Nonblch <lllskateordielll@gmail.com>
2021-09-27 19:39:56 +0300

    hw-2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,63 @@
 import  './App.css';
-import {ListGroup} from "./components/BlockOfMessage";
-import {ListGroupItem} from "./components/Message";
+import React, { useEffect, useState } from 'react';
 
 
-function App() {
+const App = () => {
+    const [messageList, setMessageList]= useState([]);
+    const [message, setMessage] = useState('');
+    const [sender, setSender] = useState('bot');
+    const [isTyping, setIsTyping] = useState(false);
+
+    useEffect(()=> {
+        if (isTyping) {
+            return;
+        }
+        loadMessage(sender);
+    }, [sender, isTyping]);
+
+    const loadMessage = () => {messageList.push({id: sender, text: message})};
+
+
+    const handleMessFormSubmit = event => {
+        event.preventDefault();
+        setIsTyping(false);
+        botForLoad();
+        setMessageList(messageList)
+    };
+
+    const botForLoad = () => {messageList.push({id: 'bot', text: 'Что ты делаешь? Не могу понять('})};
+
   return (
-    <div >
-      <ListGroup >
-        <ListGroupItem words={'big'} active={true}>?</ListGroupItem>
-        <ListGroupItem words={'great'} disabled={true}>?</ListGroupItem>
-        <ListGroupItem words={'interesting'}>\/</ListGroupItem>
-        <ListGroupItem words={'deal'}></ListGroupItem>
-      </ListGroup>
+    <div className="app">
+        <ul className="messageList">
+            {messageList.map(message => {
+                return (
+                    <li className="messageListElem">
+                        <span>
+                            {message.sender}
+                        </span>
+                        <span>
+                            {message.text}
+                        </span>
+                    </li>
+                )
+            })}
+        </ul>
+        <form onSubmit={handleMessFormSubmit}>
+            <input
+                type="text"
+                placeholder="Напиши чего-нибудь)"
+                value={message}
+                onChange={event => {
+                    setIsTyping(true);
+                    setMessage(event.target.value);
+                    setSender('user');
+                }}
+            />
+            <button>send it!</button>
+        </form>
     </div>
   );
-}
+};
 
 export default App;
